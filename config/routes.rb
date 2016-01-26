50245b02146911713710a27cf493b3ca3dc72c3a
Kyle Starratt <kyle@rentitez.com>
2016-01-26 15:35:54 -0700

    User profile accidentally made on master

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  root 'home#index'
 
   devise_for :users
+  resources :users
+
+  authenticated do
+    root to: "users#show", as: :authenticated
+  end
+  
+  root 'home#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
