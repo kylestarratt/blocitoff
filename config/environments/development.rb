5a71e599b71d21b6eecdffb666537ae1545ac46a
Kyle Starratt <kyle@rentitez.com>
2016-01-26 13:33:31 -0700

    User can sign up, sign in, sign out, and reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,6 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # added from devise instructions, Also mentioned after installing devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -41,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # added from devise instructions, Also mentioned after installing devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.delivery_method = :smtp
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
